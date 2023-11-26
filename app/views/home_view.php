2f1391fc60cf80710467e21bc8593f6648865f82
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2023-11-26 22:34:01 +0100

    home view altered

--- app/views/home_view.php
+++ app/views/home_view.php
@@ -2,10 +2,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Üdvözöljük a világot!</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="public/img/favicon.png">
+    <link rel="stylesheet" href="public/css/style.css">
+    <title>Főoldal</title>
 </head>
 <?php include 'header_view.php'; ?>
 <body>
+    <div class="container">
+        <h1>Főoldal</h1>
+    </div>
     <h1>Üdvözöljük a világot!</h1>
     <p>Ez a világ legjobb nézete!</p>
 </body>
