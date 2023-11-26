3c46039ed0b73beebb357b0b9ac145d07dfc8446
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2023-11-26 22:34:52 +0100

    alter on the login

--- app/views/login_view.php
+++ app/views/login_view.php
@@ -2,11 +2,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Üdvözöljük a világot!</title>
+    <title>Bejelentkezés</title>
 </head>
 <?php include 'header_view.php'; ?>
 <body>
-    <h1>Login</h1>
+    <div><h1>Bejelentkezés</h1></div>
+    <div>
+        <div>
+            <form action="login" method="post">
+            <input type="text" id="username" name="username" placeholder="Felhasználónév"><br>
+
+            <input type="password" id="password" name="password" placeholder="Jelszó"><br><br>
+
+            <input type="submit" value="bejelentkezes">
+            </form>
+        </div>
+    </div>
 </body>
 <?php include 'footer_view.php'; ?>
 </html>
