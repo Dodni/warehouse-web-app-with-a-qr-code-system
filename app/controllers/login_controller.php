b313162ed17bc25303116362c306d2558bf7992f
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2023-11-26 22:34:34 +0100

    alter in the login controller

--- app/controllers/login_controller.php
+++ app/controllers/login_controller.php
@@ -1,6 +1,8 @@
 <!-- login_controller.php -->
 
 <?php
+include 'app/models/user_model.php'; // Példányosítsd az User modelt
+
 class LoginController {
     public function showLoginPage() {
         $viewPath = 'app/views/login_view.php';
@@ -15,17 +17,41 @@ class LoginController {
     }
 
     public function authenticateUser($username, $password) {
-        // Ide írd meg a felhasználó azonosításához szükséges logikát
-        // Például: adatbáziskapcsolat, felhasználónév és jelszó ellenőrzése
+        // Példa: Ellenőrizzük az adatbázisból a felhasználói adatokat
+        $userModel = new UserModel(); // Példányosítsd az UserModelt
+        $user = $userModel->getUserByUsername($username); // Az adatbázisból lekérjük a felhasználót
+        
+        if ($user && password_verify($password, $user['password'])) {
+            // Sikeres bejelentkezés
+            echo 'Sikeres bejelentkezés!';
 
-        // Ha a felhasználó sikeresen bejelentkezett, tedd valamilyen további tevékenységre
-        // Például: átirányítás más oldalra vagy a belépési adatok kezelése
+            // Ide jöhet a további logika, például átirányítás más oldalra
+        } else {
+            // Sikertelen bejelentkezés
+            echo 'Hibás felhasználónév vagy jelszó!';
+        }
     }
 }
 
 // Példányosítjuk a LoginController osztályt
 $loginController = new LoginController();
 
-// Meghívjuk a showLoginPage() metódust a LoginController-ből
-$loginController->showLoginPage();
+// Ellenőrizzük, hogy POST kérés érkezett-e
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    var_dump($_SERVER["REQUEST_METHOD"]);
+    // Ellenőrizzük, hogy a felhasználónév és jelszó mezők ki lettek-e töltve
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // Authentikáció meghívása
+        $loginController->authenticateUser($username, $password);
+    } else {
+        // Hibás adatok esetén
+        echo 'Kérem, töltse ki mindkét mezőt!';
+    }
+} else {
+    // Meghívjuk a showLoginPage() metódust a LoginController-ből
+    $loginController->showLoginPage();
+}
 ?>
