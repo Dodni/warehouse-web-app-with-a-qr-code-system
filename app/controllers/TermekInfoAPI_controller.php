ced9d094e1b1d515b9f6bd13068aae6b113174e2
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2023-11-27 23:43:59 +0100

    TermekInfoAPI is full done

--- app/controllers/TermekInfoAPI_controller.php
+++ app/controllers/TermekInfoAPI_controller.php
@@ -9,26 +9,82 @@ class TermekInfoAPIController {
 
         //var_dump($termekList);
         echo $termekList;
+        return $termekList;
     }
     
     function getTermek($termekId) {
-        // Itt van a getTermek logikája a kapott $termekId alapján
-        echo "getTermek hívása a következő azonosítóval: $termekId";
+        $model = new TermekInfoAPIModel(); // Modell példányosítása
+        $termekList = $model->getTermekModel($termekId); // A modell getTermekekModel függvényének meghívása
+
+        //var_dump($termekList);
+        echo $termekList;
+        return $termekList;
     }
     
-    function postTermek($termekId) {
-        // Itt lehet a postTermek logikája
-        echo "postTermek hívása a következő azonosítóval: $termekId";
+    function postTermek($termekData) {
+        # teszt adat
+        $termekData = [
+            'termek_nev' => 'nyomdafestek',
+            'termek_szarmazas' => 'Cégnév Kft',
+            'termek_erkezesi_datum' => '2023-11-27',
+            'termek_ewc_kod' => '08 03 12',
+            'termek_darabszam' => '10',
+            'termek_suly' => '5',
+            'termek_logisztikara_kuldve' => '0'
+        ];
+        $model = new TermekInfoAPIModel(); // Modell példányosítása
+        
+        $termekList = $model->postTermekModel($termekData); // A modell getTermekekModel függvényének meghívása
+
+        //var_dump($termekList);
+        if ($termekList == true)
+        {
+            echo "Sikeres post.";
+        } else {
+            echo "Sikertelen post.";
+        }
+        //return $termekList;
     }
     
-    function putTermek($termekId) {
-        // Itt lehet a putTermek logikája
-        echo "putTermek hívása a következő azonosítóval: $termekId";
+    function putTermek($termekData) {
+        $model = new TermekInfoAPIModel(); // Modell példányosítása
+        $termekData = [
+            'termek_nev' => 'Palack',
+            'termek_szarmazas' => 'asd Kft.',
+            'termek_erkezesi_datum' => '2023.11.12',
+            'termek_ewc_kod' => '22 22 11*',
+            'termek_darabszam' => '55',
+            'termek_suly' => '66',
+            'termek_logisztikara_kuldve' => '0'
+        ];
+        
+        #$termekList = $model->getTermekModel($termekId);
+        
+        #var_dump($termekList);
+        $termekList2 = $model->putTermekModel($termekData); // A modell getTermekekModel függvényének meghívása
+        $termekData = json_encode($termekData);
+        
+        #var_dump($termekList2);
+        
+        if ($termekList2 == true)
+        {
+            echo "Sikeres put.";
+        } else {
+            echo "Sikertelen post.";
+        }
+        #return $termekList;
     }
     
     function deleteTermek($termekId) {
-        // Itt lehet a deleteTermek logikája
-        echo "deleteTermek hívása a következő azonosítóval: $termekId";
+        $model = new TermekInfoAPIModel(); // Modell példányosítása
+        $result = $model->deleteTermekModel($termekId);
+        
+        if ($result == true) {
+            echo "Sikeres törles.";
+        } else {
+            echo "Sikertelen törles.";
+        }
+        #return $result;
     }
 }
 
--- app/models/termekInfoAPI_model.php
+++ app/models/termekInfoAPI_model.php
@@ -12,5 +12,61 @@ class TermekInfoAPIModel {
         // JSON formátumra alakítás és visszaadás
         return json_encode($termekList);
     }
+
+    function getTermekModel($termekId) {
+        Database::connect();
+        $termekList = [];
+        $result = Database::$connection->query("SELECT * FROM termekek WHERE termek_id = $termekId");
+
+        while ($row = $result->fetch_assoc()) {
+            $termekList[] = $row;
+        }
+
+        // JSON formátumra alakítás és visszaadás
+        return json_encode($termekList);
+    }
+    
+    function postTermekModel($termekData) {
+        Database::connect();
+        $query = "INSERT INTO `termekek` (`termek_nev`, `termek_szarmazas`, `termek_erkezesi_datum`, `termek_ewc_kod`, `termek_darabszam`, `termek_suly`, `termek_logisztikara_kuldve`) VALUES (";
+
+        foreach ($termekData as $value) {
+            $query .= "'" . $value . "', ";
+        }
+        
+        $query = rtrim($query, ', ') . ")";
+        
+        $result = Database::$connection->query($query);
+        //var_dump($result);
+        // JSON formátumra alakítás és visszaadás
+        return json_encode($result);
+    }
+    
+    function putTermekModel($termekData) {
+        Database::connect();
+        $termekId = 1; // Példa: azonosító, amihez tartozó rekordot frissítünk
+
+        $query = "UPDATE `termekek` SET ";
+
+        foreach ($termekData as $column => $value) {
+            $query .= "`$column` = '$value', ";
+        }
+        
+        $query = rtrim($query, ', ') . " WHERE `termek_id` = $termekId";
+        #var_dump($query);
+        $result = Database::$connection->query($query);
+
+        #var_dump($result);
+        return $result;
+    }
+    
+    function deleteTermekModel($termekId) {
+        Database::connect();
+        $query = "DELETE FROM `termekek` WHERE `termek_id` = $termekId";
+        $result = Database::$connection->query($query);
+
+        var_dump($result);
+        return $result;
+    }
 }
 ?>
