1fb05aa43c221bc2e90ed8c26d2b774ed69d0dd8
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2023-11-27 11:50:46 +0100

    new views

---
+++ app/views/logout_view.php
@@ -0,0 +1,20 @@
+<!-- home_view.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="public/img/favicon.png">
+    <link rel="stylesheet" href="public/css/style.css">
+    <title>Kijelentkezve</title>
+</head>
+<?php include 'header_view.php'; ?>
+<body>
+    <div class="container">
+        <h1>Kijelentkezve</h1>
+    </div>
+    <br>
+    <a href="login">Login</a>
+</body>
+<?php include 'footer_view.php'; ?>
+</html>
---
+++ app/views/raktarkeszlet_view.php
@@ -0,0 +1,32 @@
+<!-- home_view.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="public/img/favicon.png">
+    <link rel="stylesheet" href="public/css/style.css">
+    <title><?php echo $dataSend['title']; ?></title>
+</head>
+<?php include 'header_view.php'; ?>
+<body>
+    <div class="container">
+        <h1><?php echo $dataSend['title']; ?></h1>
+    </div>
+    <br>
+    <a href="login">Login</a>
+    <br>
+    <a href="raktarkeszlet">Raktárkészlet</a>
+    <br>
+    <a href="login">Login</a>
+    <br>
+    <a href="login">Login</a>
+    <br>
+    <a href="login">Login</a>
+    <br>
+    <a href="login">Login</a>
+    <br>
+    <a href="login">Login</a>
+</body>
+<?php include 'footer_view.php'; ?>
+</html>
