25106afca5eb3a4dbcb858a2d83662f11c428957
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2023-11-28 11:28:20 +0100

    added an error view

---
+++ app/views/error_view.php
@@ -0,0 +1,18 @@
+<!-- error_view.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="public/img/favicon.png">
+    <link rel="stylesheet" href="public/css/style.css">
+    <title>Error</title>
+</head>
+<?php include 'header_view.php'; ?>
+<body>
+    <div class="container">
+        <h1>Error - 404 - Az oldal nem található</h1>
+    </div>
+</body>
+<?php include 'footer_view.php'; ?>
+</html>
