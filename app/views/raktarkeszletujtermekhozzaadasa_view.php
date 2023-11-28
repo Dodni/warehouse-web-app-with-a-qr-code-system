31b093194dc2e43718e31e85fd42e7098dd0c2e7
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2023-11-28 16:38:29 +0100

    new view

---
+++ app/views/raktarkeszletujtermekhozzaadasa_view.php
@@ -0,0 +1,21 @@
+<!-- home_view.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="public/img/favicon.png">
+    <link rel="stylesheet" href="public/css/style.css">
+    <script type="text/javascript" src="app/javascript/menu.js"></script>
+    <title>Rólunk</title>
+</head>
+<?php include 'header_view.php'; ?>
+<body>
+    <div class="container">
+        <h1>Rólunk</h1>
+        <p>Ez itt a Rólunk oldal, lehet a web alkalmazásról kicsit írni, vagy a cégről stb.</p>
+    </div>
+</body>
+<?php include 'footer_view.php'; ?>
+</html>
+
