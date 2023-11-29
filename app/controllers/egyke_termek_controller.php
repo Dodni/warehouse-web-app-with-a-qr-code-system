64b74646c9e87822f367a9f1cc15ffa26a764cbe
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2023-11-29 10:58:08 +0100

    renamed

---
+++ app/controllers/egyke_termek_controller.php
@@ -0,0 +1,38 @@
+<!-- home_controller.php -->
+
+<?php
+class EgyketermekController {
+    public function showEgykeTermekPage() {
+        
+        // Az elérési út a home_view.php fájlhoz
+        $viewPath = 'app/views/egyketermek_view.php';
+        
+        session_start();
+        // Ellenőrizzük, hogy a 'data' paraméter be van-e állítva
+        if ($_SESSION['loggedin'] == true) {
+
+            if (file_exists($viewPath)) {
+                // Betöltjük és megjelenítjük a nézetet
+                include $viewPath;
+            } else {
+                echo "A megadott nézetfájl nem található.";
+            }
+        } else {
+            // URL, amire szeretnéd küldeni az adatot
+            $dataToSend = "jelentkezzen-be-elobb"; // Az adat, amit elküldesz
+            $url = "/qr_kod_app/login?data=" . urlencode($dataToSend);
+
+            // Átirányítás a megadott URL-re
+            header("Location: $url");
+            exit;
+        }
+    }
+
+    
+
+}
+
+$controller = new EgyketermekController();
+
+$controller->showEgykeTermekPage();
+?>
