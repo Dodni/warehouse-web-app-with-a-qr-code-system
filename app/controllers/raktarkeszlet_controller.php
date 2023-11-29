b0fea0f7c1ed9d5493286c0f15ecb226caac2536
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2023-11-29 11:02:08 +0100

    bug fixed

--- app/controllers/raktarkeszlet_controller.php
+++ app/controllers/raktarkeszlet_controller.php
@@ -4,33 +4,26 @@
 include_once 'app/controllers/TermekInfoAPI_controller.php';
 class RaktarkeszletController {
     public function showRaktarkeszletPage($csoportositottTermekekArray) {
-        $dataSend = [
-            'title' => 'Raktarkészlet',
-            'sikeres'=> 'Sikeres',
-            // ... egyéb változók
-        ];
         $viewPath = 'app/views/raktarkeszlet_view.php';
         session_start();
-        #var_dump($_SESSION);
         // Ellenőrizzük, hogy a 'data' paraméter be van-e állítva
         if ($_SESSION['loggedin'] == true) {
 
             if (file_exists($viewPath)) {
                 // Betöltjük és megjelenítjük a nézetet
+                $_SESSION["datasend"] = $csoportositottTermekekArray;
                 include $viewPath;
             } else {
                 echo "A megadott nézetfájl nem található.";
             }
         } else {
-            // URL, amire szeretnéd küldeni az adatot
-            $dataToSend = "jelentkezzen-be-elobb"; // Az adat, amit elküldesz
-            $url = "/qr_kod_app/login?data=" . urlencode($dataToSend);
+            $url = "/qr_kod_app/login";
 
             // Átirányítás a megadott URL-re
             header("Location: $url");
             exit;
         }
-        $_SESSION["datasend"] = $csoportositottTermekekArray;
+        
         
     }
 
