a6173bf337bd80ff8b9fe5e91ebd5d75163a9502
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2023-11-29 18:18:25 +0100

    Update raktarkeszlet_uj_termek_hozzaadasa_view.php

--- app/views/raktarkeszlet_uj_termek_hozzaadasa_view.php
+++ app/views/raktarkeszlet_uj_termek_hozzaadasa_view.php
@@ -15,14 +15,14 @@
     <div><h1>Raktárkészlet</h1></div>
     <div><h3>Új termék hozzáadása</h3></div>
     <div class="container">
-        <form id="productForm" method="post" action="TermekInfoAPI/postTermek/" enctype="multipart/form-data">
+        <form id="productForm" method="post" action="TermekInfoAPI/postTermek/">
             <input type="text" name="termek_nev" placeholder="Termék neve" required minlength="5" maxlength="50"><br><br>
             <input type="text" name="termek_szarmazas" placeholder="Származás" required minlength="5" maxlength="50"><br><br>
             <input type="date" name="termek_erkezesi_datum" placeholder="Érkezési Dátum" required><br><br>
             <input type="text" name="termek_ewc_kod" placeholder="EWC kód" required><br><br>
             <input type="number" name="termek_darabszam" placeholder="Darab" required><br><br>
             <input type="number" name="termek_suly" step="0.01" placeholder="Súly" required min="1" max="9999"><br><br>
-            <input type="file" name="termek_kep_feltoltes" accept="image/*" capture="user" placeholder="Kép feltöltés" required><br><br>
+            <input type="text" name="termek_kep_neve" placeholder="Kép feltöltés (most kép neve)" required><br><br>
             <button type="button" id="submitForm">Termék hozzáadása</button>
             <button type="button" onclick="window.history.back()">Mégse</button>
         </form>
@@ -40,7 +40,8 @@
                 data: formData,
                 success: function(response) {
                     console.log('Sikeres kérés!', response);
-                    // Itt lehet a sikeres válasz kezelése
+                    // Sikeres válasz esetén irányíts át az elozo oldalra
+                    window.history.back();
                 },
                 error: function(xhr, status, error) {
                     console.error('Hiba a kérésben!', error);
