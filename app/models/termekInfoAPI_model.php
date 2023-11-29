3cedb3f4e7e9858d83c51426344adeef604eeb46
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2023-11-29 18:17:43 +0100

    api done

--- app/models/termekInfoAPI_model.php
+++ app/models/termekInfoAPI_model.php
@@ -28,14 +28,22 @@ class TermekInfoAPIModel {
     
     function postTermekModel($termekData) {
         Database::connect();
-        $query = "INSERT INTO `termekek` (`termek_nev`, `termek_szarmazas`, `termek_erkezesi_datum`, `termek_ewc_kod`, `termek_darabszam`, `termek_suly`, `termek_logisztikara_kuldve`) VALUES (";
+        var_dump($termekData);
+        // SQL query létrehozása az INSERT-hez
+        $query = "INSERT INTO `termekek` (`termek_nev`, `termek_szarmazas`, `termek_erkezesi_datum`, `termek_ewc_kod`, `termek_darabszam`, `termek_suly`,`termek_kep_neve`, `termek_logisztikara_kuldve`) VALUES (";
 
-        foreach ($termekData as $value) {
-            $query .= "'" . $value . "', ";
-        }
-        
-        $query = rtrim($query, ', ') . ")";
-        
+        // Változók hozzáadása a query-hez
+        $query .= "'" . $termekData['termek_nev'] . "', ";
+        $query .= "'" . $termekData['termek_szarmazas'] . "', ";
+        $query .= "'" . $termekData['termek_erkezesi_datum'] . "', ";
+        $query .= "'" . $termekData['termek_ewc_kod'] . "', ";
+        $query .= "'" . $termekData['termek_darabszam'] . "', ";
+        $query .= "'" . $termekData['termek_suly'] . "', ";
+        $query .= "'" . $termekData['termek_kep_neve'] . "', ";
+        $query .= "'0'"; // A logisztikára küldve NULL érték
+
+        $query .= ")";
+        #var_dump($query);
         $result = Database::$connection->query($query);
         //var_dump($result);
         // JSON formátumra alakítás és visszaadás
